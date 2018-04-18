f8dda74f01b9ae7ee65063e61952cb284ecb739f
Henry Conklin <hwc5gj@virginia.edu>
2018-04-18 13:55:42 -0400

    Fix settings path

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lazychef.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "facebooze.settings")
 
     from django.core.management import execute_from_command_line
 
